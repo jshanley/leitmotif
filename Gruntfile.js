f2a22946c4d89cef855e20119ffb6e64e87e3c32
John Shanley <john.michael.shanley@gmail.com>
2014-02-24 21:11:29 -0500

    set up tasks in Gruntfile, moved styles to separate folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,46 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
+		less: {
+			dev: {
+				options: {
+					paths: ['styles/less']
+				},
+				files: {
+					'styles/main.css': 'styles/less/main.less'
+				}
+			}
+		},
+
+		connect: {
+			server: {
+				options: {
+					port: 8888
+				}
+			}
+		},
+		watch: {
+			options: {
+				livereload: true
+			},
+			html: {
+				files: ['index.html']
+			},
+			js: {
+				files: ['scripts/src/**/*.js'],
+				tasks: ['gluejs']
+			},
+			less: {
+				options: {
+					livereload: false
+				},
+				files: ['styles/less/**/*.less'],
+				tasks: ['less'],
+			},
+			css: {
+				files: ['styles/main.css']
+			}
+		},
 		gluejs: {
 			build: {
 				options: {
@@ -14,6 +54,10 @@ module.exports = function(grunt) {
 		}
 	});
 	grunt.loadNpmTasks('grunt-contrib-gluejs');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-less');
 
-	grunt.registerTask('default', ['gluejs']);
+	grunt.registerTask('default', ['gluejs', 'less:dev']);
+	grunt.registerTask('preview', ['gluejs', 'less:dev', 'connect', 'watch']);
 };
